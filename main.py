344c1a3c1c4d31468c6387f57ee3b3d6adf44dfe
Валерий Елисеев <316224@itip.loc>
2021-11-27 20:46:25 +0300

    number1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 class PhoneContact():
-    pass
-
-name = PhoneContact()
-contactNumber = PhoneContact()
-note = PhoneContact()
+    def __init__(self, name, contactNumber, note):
+        self.name = name
+        self.contactNumber = contactNumber
+        self.note = note
+    def convert(self):
+        name = str(self.name)
+        contactNumber = str(self.contactNumber)
+        note = str(self.note)
+        return name, contactNumber, note
